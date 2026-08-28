9a46c802aa55b927b1915a1c5653187aaa044357
sshGurkiratSingh <appzone7092@gmail.com>
2026-08-28 22:32:13 +0530

    fix: Force Android landscape mode and allow notch cutout

--- software/controller_app/src/main.cpp
+++ software/controller_app/src/main.cpp
@@ -9,12 +9,38 @@
 
 #include <QQuickStyle>
 
+#ifdef Q_OS_ANDROID
+#include <QtCore/QJniObject>
+#include <QtCore/QCoreApplication>
+#endif
+
 int main(int argc, char *argv[])
 {
     QQuickStyle::setStyle("Material");
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
+
+#ifdef Q_OS_ANDROID
+    QNativeInterface::QAndroidApplication::runOnAndroidMainThread([]() {
+        QJniObject activity = QNativeInterface::QAndroidApplication::context();
+        if (activity.isValid()) {
+            // Force sensor landscape orientation
+            activity.callMethod<void>("setRequestedOrientation", "(I)V", 6);
+            
+            // Allow app to extend into the camera notch (cutout) area
+            QJniObject window = activity.callObjectMethod("getWindow", "()Landroid/view/Window;");
+            if (window.isValid()) {
+                window.callMethod<void>("addFlags", "(I)V", 1024); // FLAG_FULLSCREEN
+                QJniObject attrs = window.callObjectMethod("getAttributes", "()Landroid/view/WindowManager$LayoutParams;");
+                if (attrs.isValid()) {
+                    attrs.setField<jint>("layoutInDisplayCutoutMode", 1); // LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
+                    window.callMethod<void>("setAttributes", "(Landroid/view/WindowManager$LayoutParams;)V", attrs.object());
+                }
+            }
+        }
+    });
+#endif
 
     // Instantiate backend workers
     TelemetryClient telemetryClient;
