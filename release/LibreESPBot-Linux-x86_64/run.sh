3a16c16b4abb90ab12abd712f9147695aa76afd2
sshGurkiratSingh <appzone7092@gmail.com>
2026-09-06 22:09:40 +0530

    ci: Fix Android OpenCV haarcascade path and Windows MSVC detection for Ninja

--- release/LibreESPBot-Linux-x86_64/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-export LD_LIBRARY_PATH="$DIR/lib:$LD_LIBRARY_PATH"
-export QT_PLUGIN_PATH="$DIR/lib/plugins"
-export QML2_IMPORT_PATH="$DIR/lib/qml"
-export FLEXIBLAS_LIBRARY_PATH="$DIR/lib/flexiblas"
-exec "$DIR/LibreESPBot" "$@"
